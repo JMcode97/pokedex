7c1e40341b5f3f5cce00e285db7693736d5cfb51
JMcode97 <joseandresmontano@gmail.com>
2022-11-29 22:12:31 -0400

    Create types file and NewProduct progress

--- client/src/resources/pages/Products/NewProduct.tsx
+++ client/src/resources/pages/Products/NewProduct.tsx
@@ -1,9 +1,62 @@
-import React from 'react';
+import axios from 'axios';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+import { Products } from '../../types';
 
 export default function NewProduct () {
+
+    const api = 'http://localhost:8080';
+
+    const [Form, setForm] = useState<Products>({
+        title: '',
+        price: 0,
+    });
+
+    const handleForm = (e: ChangeEvent<HTMLInputElement>) => {
+        const { value, name } = e.target;
+        setForm({...Form, [name]: value});
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        console.log(Form);
+        axios.post(api + '/products', Form);
+    }
+
     return (
         <>
-            <h1>Create a new product form</h1>
+            <form
+            onSubmit={handleSubmit}
+            className='flex-col p-10' >
+                <div>
+                <label>
+                Product name
+                </label>
+                <input 
+                autoFocus
+                type='text'
+                name='title' 
+                value={Form.title}
+                className='border border-slate-500' 
+                onChange={handleForm} />
+                </div>
+
+                <div>
+                <label>
+                Product price
+                </label>
+                <input 
+                type='number'
+                name='price' 
+                value={Form.price}
+                className='border border-slate-500' 
+                onChange={handleForm} />
+                </div>
+
+                <button
+                type='submit' 
+                className='border border-teal-600 bg-teal-500 p-2 text-white' >
+                SEND
+                </button>
+            </form>
         </>
     )
 }
